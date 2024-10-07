9e1ad633337a36024660fa43ad9bf4025ecd22fd
Thabang Matlokotsi <Matlokotsijulian@gmail.com>
2024-10-07 15:39:29 +0200

    Update models.py

--- MentalHealthBlog/blog/models.py
+++ MentalHealthBlog/blog/models.py
@@ -6,7 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=100, blank=True)
-    # Add any other fields you want
+    
 
     def __str__(self):
         return self.user.username
